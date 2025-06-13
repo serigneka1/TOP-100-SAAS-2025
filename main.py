d11ca5cbf5d5994f8b14a665b105e9148ca5f50a
Serigne <kaserigne69@gmail.com>
2025-06-13 21:59:30 +0100

    Restructuration du projet avec une organisation des données et notebooks

---
+++ main.py
@@ -0,0 +1,9 @@
+from steps.get_data import get_data
+
+
+def main():
+    get_data()
+
+
+if __name__ =="__main__":
+    main()
--- steps/get_data.py
+++ steps/get_data.py
@@ -1,4 +1,5 @@
 import kagglehub
+import logging
 
 import pandas as pd
 import os
@@ -7,8 +8,18 @@ import os
 path = kagglehub.dataset_download("shreyasdasari7/top-100-saas-companiesstartups")
 
 def get_data() -> pd.DataFrame:
-    df = pd.read_csv(os.path.join(path, "top_100_saas_companies_2025.csv"))
-    df.to_csv("./data/top_100_saas_companies_2025.csv")
-    return df
-
+    try:
+        file_name = "top_100_saas_companies_2025.csv"
+        df = pd.read_csv(os.path.join(path,file_name ))
+        logging.info("Le dataset est bien trouvé....")
+        saving_path = "./data/raw"
+        if not os.path.exists(saving_path):
+            os.makedirs(saving_path)
+            logging.info("Répertoire /data/raw est créé ...")
+        df.to_csv(os.path.join(saving_path, file_name))
+        logging.info("Le dataset est bien chargé dans data/raw")
+        return df
+    except Exception as e:
+        logging.error(f"Il y a une erreur lors de la collecte du dataset: {e}")
+        raise e 
 
